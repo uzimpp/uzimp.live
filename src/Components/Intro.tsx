2f4a440d63494d8c21c690f8df5aa3b66832eb01
Worakrit Kullanatpokin <130132510+uzimpp@users.noreply.github.com>
2023-06-05 22:33:46 +0700

    Update Intro.tsx

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -11,7 +11,7 @@ function Intro() {
           <Group position="apart">
             <div>
               <Title className={styles.intro}>Ahola, my name is</Title>
-              <Title className={styles.name} size="h1" color="#5A9DD3">
+              <Title className={styles.name} size="h1" color="#82AFD3">
                 Boss <span className={styles.subname}>aka uzimp</span>
               </Title>
             </div>
